7a650204b68470f38616d88804dc4533d7963c38
Alison Lai <158998700+lai-jy@users.noreply.github.com>
2025-02-26 18:06:43 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         // Unit Test for Cash class
 
-        // Test setup
-        Exchange exchange = new NYSE("someAccessKey");
+        // Use FakeExchange instead of NYSE
+        Exchange exchange = new FakeExchange();
         Cash dollar = new Cash(exchange, 100);
         System.out.println("Dollar: " + dollar.toString());
 
